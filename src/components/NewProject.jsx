499d4d1412a9e2fe8523746be4ce93d921f89086
Mahdi Ghaharian <mahdighaharian@MBP-MGH.local>
2024-03-18 02:17:41 +0400

    styling the error modal and handling click event of cancel button

--- src/components/NewProject.jsx
+++ src/components/NewProject.jsx
@@ -3,7 +3,7 @@ import { useRef } from 'react';
 import Input from "./Input.jsx";
 import Modal from './Modal.jsx';
 
-export default function NewProject({ onAdd }) {
+export default function NewProject({ onAdd, onCancel }) {
     const modal = useRef();
 
     const title = useRef();
@@ -35,15 +35,16 @@ export default function NewProject({ onAdd }) {
     return (
         <>
             <Modal ref={modal} buttonCaption="Close">
-                <h2>Invalid input</h2>
-                <p>Oops...looks like you forgot to enter a value.</p>
-                <p>Please make sure to provide a valid value for every input fields.</p>
+                <h2 className="text-xl font-bold text-stone-700 my-4">Invalid input</h2>
+                <p className="text-stone-600 mb-4">Oops...looks like you forgot to enter a value.</p>
+                <p className="text-stone-600 mb-4">Please make sure to provide a valid value for every input fields.</p>
             </Modal>
             <div className="w-[35rem] mt-16">
                 <menu className="flex items-center justify-end gap-4 my-4">
                     <li>
                         <button
-                            className="text-stone-800 hover:text-stone-950">
+                            className="text-stone-800 hover:text-stone-950"
+                            onClick={onCancel}>
                             Cancel
                         </button>
                     </li>
