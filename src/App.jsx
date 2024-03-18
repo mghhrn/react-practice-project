1580023c5252e98da8aadcb9999dc223a51ac2b5
Mahdi Ghaharian <mahdighaharian@MBP-MGH.local>
2024-03-18 13:53:32 +0400

    handling delete proejct

--- src/App.jsx
+++ src/App.jsx
@@ -54,11 +54,19 @@ function App() {
     });
   }
 
-  console.log(projectsState);
+  function handleDeleteProject() {
+    setProjectsState(prevState => {
+      return {
+        ...prevState,
+        selectedProjectId: undefined,
+        projects: prevState.projects.filter((project) => project.id !== prevState.selectedProjectId)
+      };
+    });
+  }
 
   const selectedProject = projectsState.projects.find(p => p.id === projectsState.selectedProjectId);
 
-  let content = <SelectedProject project={selectedProject} />;
+  let content = <SelectedProject project={selectedProject} onDelete={handleDeleteProject} />;
 
   if (projectsState.selectedProjectId === null) {
     content = <NewProject onAdd={handleAddProject} onCancel={handleCancelAddProject} />
