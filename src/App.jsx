4e0777712ae860f15e7ff9c575d89ae873b7b692
Mahdi Ghaharian <mahdighaharian@MBP-MGH.local>
2024-03-19 01:52:44 +0400

    task deletion and some minor bug fixes

--- src/App.jsx
+++ src/App.jsx
@@ -28,8 +28,15 @@ function App() {
     });
   }
 
-  function handleDeleteTask() {
-
+  function handleDeleteTask(id) {
+    setProjectsState(prevState => {
+      return {
+        ...prevState,
+        tasks: prevState.tasks.filter(
+          (task) => task.id !== id
+        )
+      };
+    });
   }
 
   function handleSelectProject(id) {
@@ -106,7 +113,9 @@ function App() {
       <ProjectsSidebar
         onStartAddProject={handleStartAddProject}
         projects={projectsState.projects}
-        onSelectProject={handleSelectProject} />
+        onSelectProject={handleSelectProject} 
+        selectedProjectId={projectsState.selectedProjectId}
+        />
       {content}
     </main>
   );
